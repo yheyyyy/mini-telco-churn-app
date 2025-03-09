ff72e1bb4537d4d91cfe0d949180d15d1e8e7b80
Onn Yun Hui <onnyunhui@gmail.com>
2025-03-09 23:52:49 +0800

    Refactor app, input processing, and model for customer churn prediction

--- src/input_processing.py
+++ src/input_processing.py
@@ -1,32 +1,40 @@
-def format_model_inputs(input_dict):
-    age = int(input_dict['age'])
-    sex = input_dict['sex']
-    bmi = float(input_dict['bmi'])
-    children = int(input_dict['children'])
-    smoker = input_dict['smoker']
-    region = input_dict['region']
+def format_model_inputs(dict):
+    contract_type = int(dict['contract_type'])
+    num_referrals = int(dict['num_referrals'])
+    num_dependents = int(dict['num_dependents'])
+    tenure_months = int(dict['tenure_months'])
+    internet_type = int(dict['internet_type'])
+    total_monthly_fee = float(dict['total_monthly_fee'])
+    age = int(dict['age'])
+    avg_gb_download_monthly = int(dict['avg_gb_download_monthly'])
+    total_charges_quarter = float(dict['total_charges_quarter'])
+    area_id = int(dict['area_id'])
 
-    return [age, sex, bmi, children, smoker, region]
+    return [contract_type, num_referrals, num_dependents, tenure_months,
+    internet_type,total_monthly_fee, age, avg_gb_download_monthly, 
+    total_charges_quarter, area_id]
 
 
 def validate(input_dict):
     errors = []
-
-    required_fields = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
+    
+    required_fields= ['contract_type', 'num_referrals', 'num_dependents', 'tenure_months',
+    'internet_type','total_monthly_fee', 'age', 'avg_gb_download_monthly', 
+    'total_charges_quarter', 'area_id']
 
     for field in required_fields:
 
         if field not in input_dict:
             errors.append(f'{field} not found in request.')
 
-        if field in ['age', 'children'] and type(input_dict[field]) != int \
+        if field in ['tenure_months', 'age', 'avg_gb_download_monthly', 'area_id'] and type(input_dict[field]) != int \
                 and not input_dict[field].isnumeric():
-            errors.append(f'Age and children fields must be int type.')
+            errors.append(f'Field {field} must be int type.')
 
-        elif field == 'bmi' and type(input_dict[field]) != float:
+        elif field in ['total_monthly_fee', 'total_charges_quarter'] and type(input_dict[field]) != float:
             try:
-                float(input_dict['bmi'])
+                float(input_dict[field])
             except ValueError:
-                errors.append(f'bmi field must be numeric.')
+                errors.append(f'Field {field} must be numeric.')
 
     return errors
